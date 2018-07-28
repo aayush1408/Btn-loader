a5f31c42cb0bf41338a65f9aff0d5a97f43d0e48
aayush1408 <aayushjaiswal0114@gmail.com>
2018-07-28 18:08:18 +0530

    size functionality added

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 (function () {
+
     var getButton = document.getElementsByClassName('btn-loader') || []
     var elementArray = [].slice.call(getButton);
     elementArray.forEach(function (element) {
         element.addEventListener('click', handleClick)
     })
+
     function handleClick(event) {
         var timer = this.getAttribute('timer')
+        var size = this.getAttribute('size')
+        console.log(size)
         console.log(timer)
         this.style.display = 'None'
         var tempDiv = document.createElement('div')
         tempDiv.classList.add('loader')
+        if (size === 'sm') {
+            tempDiv.classList.add('sm-loader')
+        }
+        else if (size === 'md') {
+            tempDiv.classList.add('md-loader')
+        }
+        else if (size === 'lg') {
+            tempDiv.classList.add('lg-loader')
+        }
         this.insertAdjacentElement('beforebegin', tempDiv);
         var that = this
         setTimeout(function () {
@@ -17,4 +30,5 @@
             tempDiv.style.display = 'None'
         }, timer * 1000);
     }
+
 })()
