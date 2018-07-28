c6e36b657459da7ff95cd6994f4a69797a5cf7dc
aayush1408 <aayushjaiswal0114@gmail.com>
2018-07-29 01:01:28 +0530

    after text property added

--- app.js
+++ app.js
@@ -1,71 +1,77 @@
 (function () {
 
-    var getButton = document.getElementsByClassName('btn-loader') || []
-    var elementArray = [].slice.call(getButton);
-    elementArray.forEach(function (element) {
-        element.addEventListener('click', handleClick)
-    })
+	var getButton = document.getElementsByClassName('btn-loader') || []
+	var elementArray = [].slice.call(getButton)
+	elementArray.forEach(function (element) {
+		element.addEventListener('click', handleClick);
+	})
 
-    function handleClick(event) {
+	function handleClick(event) {
 
-        var timer = this.getAttribute('timer') || 60
-        var size = this.getAttribute('size')
-        var thickness = this.getAttribute('thickness')
-        var headOfLoader = this.getAttribute('color-head')
-        var bckOfLoader = this.getAttribute('bck-color')
-        var bottomOfLoader = this.getAttribute('color-bottom')
-        var rightOfLoader = this.getAttribute('color-right')
-        var leftOfLoader = this.getAttribute('color-left')
-        this.style.display = 'None'
+		var timer = this.getAttribute('timer') || 60
+		var size = this.getAttribute('size')
+		var thickness = this.getAttribute('thickness')
+		var headOfLoader = this.getAttribute('color-head')
+		var bckOfLoader = this.getAttribute('bck-color')
+		var bottomOfLoader = this.getAttribute('color-bottom')
+		var rightOfLoader = this.getAttribute('color-right')
+		var leftOfLoader = this.getAttribute('color-left')
+		var textAfter = this.getAttribute('text-after')
 
-        var tempDiv = document.createElement('div')
-        tempDiv.classList.add('loader')
+		this.style.display = 'None'
 
-        if (size === 'sm') {
-            tempDiv.classList.add('sm-loader')
-        }
-        else if (size === 'md') {
-            tempDiv.classList.add('md-loader')
-        }
-        else if (size === 'lg') {
-            tempDiv.classList.add('lg-loader')
-        }
-        else if (size === 'xl') {
-            tempDiv.classList.add('xl-loader')
-        }
+		var tempDiv = document.createElement('div')
+		tempDiv.classList.add('loader')
 
-        if (thickness) {
-            tempDiv.style.borderWidth = thickness.toString() + 'px';
-            tempDiv.style.borderTopWidth = thickness.toString() + 'px';
-        }
+		if (size === 'sm') {
+			tempDiv.classList.add('sm-loader')
+		}
+		else if (size === 'md') {
+			tempDiv.classList.add('md-loader')
+		}
+		else if (size === 'lg') {
+			tempDiv.classList.add('lg-loader')
+		}
+		else if (size === 'xl') {
+			tempDiv.classList.add('xl-loader')
+		}
 
-        if (bckOfLoader) {
-            tempDiv.style.borderColor = bckOfLoader.toString()
-        }
+		if (thickness) {
+			tempDiv.style.borderWidth = thickness.toString() + 'px'
+			tempDiv.style.borderTopWidth = thickness.toString() + 'px'
+		}
 
-        if (headOfLoader) {
-            tempDiv.style.borderTopColor = headOfLoader.toString()
-        }
+		if (bckOfLoader) {
+			tempDiv.style.borderColor = bckOfLoader.toString()
+		}
 
-        if (bottomOfLoader) {
-            tempDiv.style.borderBottomColor = bottomOfLoader.toString()
-        }
+		if (headOfLoader) {
+			tempDiv.style.borderTopColor = headOfLoader.toString()
+		}
 
-        if (rightOfLoader) {
-            tempDiv.style.borderRightColor = rightOfLoader.toString()
-        }
+		if (bottomOfLoader) {
+			tempDiv.style.borderBottomColor = bottomOfLoader.toString()
+		}
 
-        if (leftOfLoader) {
-            tempDiv.style.borderLeftColor = leftOfLoader.toString()
-        }
+		if (rightOfLoader) {
+			tempDiv.style.borderRightColor = rightOfLoader.toString()
+		}
 
-        this.insertAdjacentElement('beforebegin', tempDiv);
+		if (leftOfLoader) {
+			tempDiv.style.borderLeftColor = leftOfLoader.toString()
+		}
 
-        var that = this
-        setTimeout(function () {
-            that.style.display = 'inline'
-            tempDiv.style.display = 'None'
-        }, timer * 1000);
-    }
+		this.insertAdjacentElement('beforebegin', tempDiv);
+
+		var that = this
+		setTimeout(function () {
+			if (textAfter) {
+				that.style.display = 'inline'
+				that.innerHTML = textAfter.toString()
+			}
+			that.style.display = 'inline'
+			tempDiv.style.display = 'None'
+		}, timer * 1000);
+	}
 
 })()
